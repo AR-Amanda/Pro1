58cbd371e52ad7f1c7aaba4cd9d399cfbca22409
AR-Amanda <amandi65@gmail.com>
2023-08-07 19:01:17 -0300

    exerciciosVariaveis

---
+++ ex2Variaveis.js
@@ -0,0 +1,9 @@
+//Variavel mutavel
+let populacaoDaCidade = 200_000;
+
+console.log(populacaoDaCidade);
+
+// Troca de valor na variavel
+populacaoDaCidade = 190_000;
+
+console.log(populacaoDaCidade);
