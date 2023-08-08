d90959587e1c784784f678e97a0e8fbe14010c21
AR-Amanda <amandi65@gmail.com>
2023-08-08 18:25:02 -0300

    exValorDaCompra

--- ex5ValorDaCompra.js
+++ ex5ValorDaCompra.js
@@ -1,6 +1,6 @@
 const valorDaCompra = 100;
 
-const numeroDeParcelas = 3;
+const numeroDeParcelas = 10;
 
 
 if (numeroDeParcelas === 1) {
@@ -10,10 +10,22 @@ if (numeroDeParcelas === 1) {
 
     console.log(`Você deve pagar ${valorApagar} reais, à vista tem 10% desconto`);
 }
-else {
+else if (numeroDeParcelas >= 2 && numeroDeParcelas <= 6) {
 
     const valorDaParcela = (valorDaCompra / numeroDeParcelas).toFixed(2);
 
-    console.log(`Você deve pagar ${numeroDeParcelas} no valor ${valorDaParcela} reais`);
+    console.log(`Você deve pagar ${numeroDeParcelas} no valor ${valorDaParcela} reais, sem juros`);
 
+}
+else if (numeroDeParcelas >= 7 && numeroDeParcelas <= 12) {
+
+    const valorJuros = valorDaCompra * (1 + 1 / 100) ** numeroDeParcelas;
+
+    const valorDaParcela = (valorJuros / numeroDeParcelas).toFixed(2);
+
+    console.log(`Você deve pagar ${numeroDeParcelas} no valor ${valorDaParcela} reais, com juros`);
+
+}
+else {
+    console.log("Total de parcelas inválida.");
 }
