7acd487dfbaba7a0114c3fa65cc8407246e0eb72
AR-Amanda <amandi65@gmail.com>
2023-08-09 17:55:10 -0300

    Array Paises

--- 4arrayLoops/ex7ArraysPaises.js
+++ 4arrayLoops/ex7ArraysPaises.js
@@ -1,22 +1,25 @@
 //Crie um array de paises
 const paises = ['Gana', 'Níger', 'Egito', 'Congo', 'Líbia'];
+console.log(paises);
 
 //Adiciona no final do array
 paises.push('Mali');
+console.log(paises);
 
 //Remove o último
 paises.pop();
+console.log(paises);
 
 //Adiciona no inicio do array
 paises.unshift('Mali');
+console.log(paises);
 
 //Remove o primeiro
 paises.shift();
+console.log(paises);
 
 //Último do array
 const ultimo = paises[paises.length - 1];
-
-console.log(paises);
 
 console.log(ultimo);
 //Segundo na tela
