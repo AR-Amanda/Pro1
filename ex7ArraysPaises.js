8ac3860e2890bcea6f16d75660fb8a199500762e
AR-Amanda <amandi65@gmail.com>
2023-08-09 17:33:06 -0300

    Array Paises

--- ex7ArraysPaises.js
+++ ex7ArraysPaises.js
@@ -13,4 +13,13 @@ paises.unshift('Mali');
 //Remove o primeiro
 paises.shift();
 
+//Último do array
+const ultimo = paises[paises.length - 1];
+
 console.log(paises);
+
+console.log(ultimo);
+//Segundo na tela
+console.log(paises[1]);
+//Índice 2 na tela
+console.log(paises[2]);
