ecd4f1dc488a19462f811bb463bb81bf9d90ea3d
AR-Amanda <amandi65@gmail.com>
2023-08-09 18:19:05 -0300

    ex2Var

--- 2variaveis/ex2Variaveis.js
+++ 2variaveis/ex2Variaveis.js
@@ -3,7 +3,7 @@ let populacaoDaCidade = 200_000;
 
 console.log(populacaoDaCidade);
 
-// Troca de valor na variavel
+// Troca de valor na variavel.
 populacaoDaCidade = 190_000;
 
 console.log(populacaoDaCidade);
