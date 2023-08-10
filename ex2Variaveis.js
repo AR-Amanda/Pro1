df62896bde59d82748c207f2d37915a0833e545a
AR-Amanda <141366123+AR-Amanda@users.noreply.github.com>
2023-08-09 21:26:01 -0300

    Delete ex2Variaveis.js

--- ex2Variaveis.js
+++
@@ -1,9 +0,0 @@
-//Variavel mutavel
-let populacaoDaCidade = 200_000;
-
-console.log(populacaoDaCidade);
-
-// Troca de valor na variavel
-populacaoDaCidade = 190_000;
-
-console.log(populacaoDaCidade);
