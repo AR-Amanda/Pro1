b75d945f476835b1d413ad2f69bb58461b40b5e5
AR-Amanda <amandi65@gmail.com>
2023-08-18 20:24:23 -0300

    funcoes

--- 6funcoes/exeFuncoes.js
+++ 6funcoes/exeFuncoes.js
@@ -1,12 +1,37 @@
 //exer 1
 
-function p(pessoa) {
-    console.log(`Olá! Meu nome é ${pessoa.nome}, sou um jovem de ${pessoa.idade} anos, ${pessoa.altura} de altura e sou ${pessoa.profissao}!`);
-}
-
-p(pessoa = {
+const c = {
     nome: "Maria",
     idade: 30,
     profissao: "atriz",
     altura: 1.67
-})
+}
+
+function pessoaFrase(pessoa) {
+    console.log(`Olá! Meu nome é ${c.nome}, sou um jovem de ${c.idade} anos, ${c.altura} de altura e sou ${c.profissao}!`);
+}
+
+pessoaFrase(c);
+
+
+//exer 2
+
+
+function faixaEtaria(idade) {
+    if (idade <= 21) {
+
+        return "jovem";
+
+    } else if (idade < 66) {
+
+        return "adulto(a)";
+
+
+    } else {
+
+        return "Idoso(a)";
+    }
+}
+
+console.log(faixaEtaria(16));
+
