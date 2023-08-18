907a0b97c51afe6011a89aa251b18c28a927db59
AR-Amanda <amandi65@gmail.com>
2023-08-18 19:57:09 -0300

    funcoes

--- 6funcoes/exeFuncoes.js
+++ 6funcoes/exeFuncoes.js
@@ -1,7 +1,7 @@
 //exer 1
 
 function p(pessoa) {
-    console.log(`Olá! Meu nome é ${pessoa.nome}, sou um jovem de ${pessoa.idade}, ${pessoa.altura} de altura e sou ${pessoa.profissao}!`);
+    console.log(`Olá! Meu nome é ${pessoa.nome}, sou um jovem de ${pessoa.idade} anos, ${pessoa.altura} de altura e sou ${pessoa.profissao}!`);
 }
 
 p(pessoa = {
