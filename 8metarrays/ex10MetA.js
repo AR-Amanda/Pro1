bc77a15cdc40e9077d3b5ec54e04eae57511fcfa
AR-Amanda <amandi65@gmail.com>
2023-08-25 18:16:45 -0300

    metodosArrays

--- 8metarrays/ex10MetA.js
+++ 8metarrays/ex10MetA.js
@@ -1,5 +1,6 @@
+//exer1
 const usuarios = [
-    { id: 21, nome: 'joao', idade: 23 },
+    { id: 21, nome: 'joao', idade: 33 },
     { id: 2, nome: 'maria', idade: 18 },
     { id: 4, nome: 'ana', idade: 30 },
     { id: 1, nome: 'rodrigo', idade: 17 },
@@ -17,3 +18,18 @@ usuarios.sort((primeiro, segundo) => {
 });
 
 console.log(usuarios);
+
+
+//exer3
+
+const idadeMaior = usuarios.reduce((acumulador, elemento) => {
+    let maior = acumulador;
+    if (elemento.idade > maior.idade) {
+        maior = elemento;
+
+    }
+    return maior;
+
+});
+
+console.log(idadeMaior);
